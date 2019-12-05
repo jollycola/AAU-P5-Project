b4c574e227ca8131e89d54fdd53a53bfd8d1a0d2
Mads-Bo Lassen <mlass17@student.aau.dk>
2019-12-05 11:26:26 +0100

    Cleanup

--- raspi/comm_test.py
+++
@@ -1,6 +0,0 @@
-from communication.client import Client
-
-
-client = Client("192.168.1.123", 6969)
-client.connect_to_server()
-
--- raspi/start
+++
@@ -1,1 +0,0 @@
-LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0 python3 main.py
