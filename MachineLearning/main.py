e9fdb110aef9378849901e30fafac5c73eb3a14c
Mathias Andresen <dev@andresen.xyz>
2019-12-09 09:59:52 +0100

    setup

--- MachineLearning/main.py
+++ MachineLearning/main.py
@@ -9,21 +9,21 @@ from sklearn.model_selection import train_test_split
 #from math import sqrt
 
 # Open data_sheet
-data_sheet = pd.read_excel('stats.xlsx')
+data_sheet = pd.read_excel('training-set.xlsx')
 
-target_columns = ['POWER', 'SVING']
-removed_columns = ['POWER', 'SVING', '#SLAG']
+target_columns = ['POWER', 'SWING']
+removed_columns = ['STROKE']
 
 # Data_sheet we work with to predict
 input_data_sheet = list(set(list(data_sheet.columns))-set(removed_columns))
 
 # Normalizing data_sheet
-#data_sheet[input_data_sheet] = data_sheet[input_data_sheet]/data_sheet[input_data_sheet].max() 
+# data_sheet[input_data_sheet] = data_sheet[input_data_sheet]/data_sheet[input_data_sheet].max() 
 
 X = data_sheet[input_data_sheet].values
-y = data_sheet[target_columns].values
+Y = data_sheet[target_columns].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=40)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random_state=40)
 
 # Building model
 model = Sequential()
@@ -35,5 +35,5 @@ model.add(Dense(2))
 model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
 
 # Training for K-epochs
-model.fit(X_train, y_train, epochs=20)
+model.fit(x_train, y_train, epochs=50)
 model.save('minigolf-network.model')
